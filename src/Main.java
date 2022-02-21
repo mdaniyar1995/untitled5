d294fa8fcd1e87e2f3b8495fc05d6dedd6027ab7
Daniyar <mdaniyar1995@gmail.com>
2022-02-22 01:54:55 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        Scanner in = new Scanner(new FileInputStream("src/birthday.txt"));
-        System.out.println(in.nextInt());
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("src/text.txt");
+        int i;
+        while ((i = fileInputStream.read()) != -1) {
+            System.out.print((char) i);
+        }
     }
-
 }
